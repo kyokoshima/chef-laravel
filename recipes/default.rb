f95d5100432fb926929fac3aaba59d00bce93d8f
kyokoshima <dev.kyoko@gmail.com>
2014-06-10 00:01:30 +0900

    composerインストールまで追加

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,15 @@
 #
 
 include_recipe "php::source"
+
+execute "composer_install" do
+	command <<-EOH
+		curl -sS https://getcomposer.org/installer | php
+		mv composer.phar /usr/local/bin/composer
+	EOH
+	#action :nothing
+end
+
+package "curl" do
+	notifies :run, resources(:execute => :composer_install)
+end
